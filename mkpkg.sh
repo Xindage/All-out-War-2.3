91b0329a7bab1b379af29ed39b814a0d099d35e6
xindage <xindage@gmail.com>
2022-11-12 13:10:02 -0300

    Lot of fixes. BTOs take less damage from ion/nuke. Players drop grenades on death if he has an active grenade on hand. #6 Fixed flashbang error in acs. mkpkg.sh now generate no compression pk3 to prevent issues, also fixed missing animdefs in the files. lightsaber skin restored. fixed soundinfo data.

--- mkpkg.sh
+++ mkpkg.sh
@@ -6,21 +6,21 @@ echo "Obtaining date variable."
 vardate=$(date +"%y%m%d")
 echo "Result is : $vardate"
 
-# Generate data pk7.
+# Generate data pk3.
 # Here is compressed all visual and auditive data of the game but musics.
-7z a OUTPUT/aow2.3_data_r$vardate.pk7 animadef.txt colormap.dat dbigfont.lmp decaldef.txt fontdefs.txt hirestex.txt hirestex.weapons.txt keyconf.txt modeldef.txt notch.dat playpal.pal sbarinfo.txt skininfo.txt sndinfo.txt sndseq.txt startup.dat terrain.txt textures.iwadfix.txt textures.txt -r textures/* sprites/* sounds/* patches/* models/* graphics/* flats/* -m0=copy
+zip -Ar0 OUTPUT/aow2.3_data_r$vardate.pk3 animdefs.txt colormap.dat dbigfont.lmp decaldef.txt fontdefs.txt hirestex.txt hirestex.weapons.txt keyconf.txt modeldef.txt notch.dat playpal.pal sbarinfo.txt skininfo.txt sndinfo* sndseq.txt startup.dat terrain.txt textures.iwadfix.txt textures.txt textures/* sprites/* sounds/* patches/* models/* graphics/* flats/*
 
-# Generate code pk7.
+# Generate code pk3.
 # Here is compressed all the actors and acs from the source into the file.
 echo "Generating code package."
 # Compile the acs.
 acc source/aow2scrp.acs acs/aow2scrp.o
 echo "ACS source was compiled successfully!"
-7z a OUTPUT/aow2.3_code_r$vardate.pk7 cvarinfo.txt decorate.txt gameinfo.txt gldefs.txt language.txt loadacs.txt teaminfo.txt TextColours.txt TextColours2.txt TextColours3.txt TextColours4.txt TextColours5.txt -r acs/* source/* actors/* credits/* -m0=copy
+zip -Ar0 OUTPUT/aow2.3_code_r$vardate.pk3 cvarinfo.txt decorate.txt gameinfo.txt gldefs.txt language.txt loadacs.txt teaminfo.txt TextColours.txt TextColours2.txt TextColours3.txt TextColours4.txt TextColours5.txt  acs/* source/* actors/* credits/*
 
-# Generate maps pk7.
+# Generate maps pk3.
 echo "Generating maps package."
-7z a OUTPUT/aow2.3_maps_r$vardate.pk7 mapinfo.txt maps/* credmaps/* -m0=copy
+zip -0 OUTPUT/aow2.3_maps_r$vardate.pk3 mapinfo.txt maps/* credmaps/*
 
-# Generate music pk7.
-7z a OUTPUT/aow2.3_music_r$vardate.pk7 music/* credmus/* -m0=copy
+# Generate music pk3.
+zip -0 OUTPUT/aow2.3_music_r$vardate.pk3 music/* credmus/*
